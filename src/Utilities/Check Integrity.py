702d1321b709fc9fdd2756972cc06ab1f1274965
Hans Bakker <jjabakker@icloud.com>
2025-04-23 12:01:38 +0200

    Moved from Application to Utilities

--- src/Utilities/Check Integrity.py
+++ src/Utilities/Check Integrity.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
-from src.Application.Check_Integrity.Check_integrity_Dialog import CheckIntegrityDialog
-from src.Application.Utilities.General_Support_Functions import set_application_icon
+from src.Utilities.Check_Integrity.Check_integrity_Dialog import CheckIntegrityDialog
+from src.Application.Support.General_Support_Functions import set_application_icon
 
 if __name__ == "__main__":
     root = tk.Tk()
