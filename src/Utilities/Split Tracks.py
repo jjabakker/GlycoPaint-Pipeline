69bc3e8f49c26706a72ca98152b83601ce191bd3
Hans Bakker <jjabakker@icloud.com>
2025-07-30 08:13:38 +0200

    Update reporting Spaces allowed

--- src/Utilities/Split Tracks.py
+++ src/Utilities/Split Tracks.py
@@ -6,6 +6,7 @@ from src.Fiji.LoggerConfig import paint_logger
 def split_tracks_by_cell_type(directory):
 
     paint_logger.info("Splitting tracks by cell type")
+    paint_logger.info("")
 
     paint_logger.info("reading All Recordings ")
     df_recordings = pd.read_csv(f"{directory}/All Recordings.csv")
@@ -13,10 +14,12 @@ def split_tracks_by_cell_type(directory):
     paint_logger.info("Reading All Tracks")
     df_all_tracks = pd.read_csv(f"{directory}/All Tracks.csv")
 
+    paint_logger.info("")
+
     unique_cell_types = df_recordings['Cell Type'].dropna().unique().tolist()
     for cell_type in unique_cell_types:
         paint_logger.info(f"Splitting tracks for {cell_type}...")
-        safe_cell_type = cell_type.replace('/', '').replace('\\', '').replace(' ', '')
+        safe_cell_type = cell_type.replace('/', '').replace('\\', '').replace(' ', ' ')
         recording_names_list = df_recordings[df_recordings['Cell Type'] == cell_type]['Ext Recording Name'].dropna().unique().tolist()
         df_all_tracks_for_cell_type = df_all_tracks[df_all_tracks['Ext Recording Name'].isin(recording_names_list)]
         if not df_all_tracks_for_cell_type.empty:
